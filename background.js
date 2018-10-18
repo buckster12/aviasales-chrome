246e0de2389065df918f4c15381dca6b56b0c984
buckster12 <roboks@mail.ru>
2018-10-18 08:15:26 +0300

    window opens alerts (cron analog)

--- background.js
+++ background.js
@@ -1,8 +1,30 @@
 // event: called when extension is installed or updated or Chrome is updated
-function onInstalled() {
-    // CREATE ALARMS HERE
-// ...
+function onInstalled() { }
+
+function onAlarm(alarm) {
+    // console.log('alarm');
+    console.log(alarm);
+
+    // switch (alarm.name) {
+    //     case 'updatePhotos':
+    //         break;
+    //
+    //     default:
+    //         break;
+    // }
+
+    var betweenDates = ['01.02', '31.03'];
+
+
+    var dateFrom = '0802';
+    var dateTo = '2102';
+    var from = "MOW";
+    var to = "CMB";
+
+    var action_url = "https://www.aviasales.ru/search/"+from+dateFrom+to+dateTo+"1";
+    chrome.tabs.create({ url: action_url });
 }
+
 
 // event: called when Chrome first starts
 function onStartup() {
@@ -10,21 +32,6 @@ function onStartup() {
 // ...
 }
 
-// event: alarm raised
-function onAlarm(alarm) {
-
-    console.log('FIRE FIRE FIRE');
-
-    switch (alarm.name) {
-        case 'updatePhotos':
-            // get the latest for the live photo streams
-            // photoSources.processDaily();
-            break;
-        // ...
-        default:
-            break;
-    }
-}
 
 // listen for extension install or update
 chrome.runtime.onInstalled.addListener(onInstalled);
--- popup.js
+++ popup.js
@@ -1,20 +1,14 @@
 $(document).ready(function () {
 
-    chrome.alarms.onAlarm.addListener(onAlarm);
-
-    function onAlarm() {
-        var dateFrom = '0802';
-        var dateTo = '2102';
-        var from = "MOW";
-        var to = "CMB";
-
-        var action_url = "https://www.aviasales.ru/search/"+from+dateFrom+to+dateTo+"1";
-        chrome.tabs.create({ url: action_url });
-    }
+    // chrome.alarms.onAlarm.addListener(onAlarm);
 
     $('#buttonTest').on('click', function () {
 
-        // chrome.tabs.create('sdfdsf');
+
+        // chrome.alarms.create('checkNewTasks', {
+            // when: 1000,
+            // periodInMinutes: 1
+        // });
 
         var dateFrom = '0802';
         var dateTo = '2102';
@@ -23,28 +17,30 @@ $(document).ready(function () {
 
         var action_url = "https://www.aviasales.ru/search/"+from+dateFrom+to+dateTo+"1";
         chrome.tabs.create({ url: action_url });
+    });
 
-        // var openedWindow = window.open('https://www.aviasales.ru/search/MOW0802CMB19021', '_blank');
-        // if (openedWindow) {
-        //     Browser has allowed it to be opened
-            // openedWindow.focus();
-        // } else {
-        //     Browser has blocked it
-            // alert('Please allow popups for this website');
-        // }
+    $('#saveSettings').on('click', function () {
+        var duration = $('#durationInput').val();
 
+        chrome.storage.local.set({duration: duration}, function() {
+            $('#result').html('<span>saved</span>');
+            setTimeout(function () {
+                $('#result').find("span").fadeOut('slow')
+            }, 200);
+
+            // console.log('Value is set to ' + duration);
+        });
     });
 
 });
 
-// function checkNewTasks() {
-//     'use strict';
-//     console.log('started');
-//     var action_url = "https://www.aviasales.ru/search/"+from+dateFrom+to+dateTo+"1";
-//     chrome.tabs.create({ url: action_url });
-// }
+// chrome.storage.local.set({key: value}, function() {
+//     console.log('Value is set to ' + value);
+// });
 
-chrome.alarms.create('checkNewTasks', {
-    // when: 1000,
-    periodInMinutes: 1
+chrome.storage.local.get(['duration'], function(result) {
+    console.log('Value currently is ' + result.key);
 });
+
+// Run alarm every minute
+// chrome.alarms.create('checkNewTasks', { periodInMinutes: 5 });
