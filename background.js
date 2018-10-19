cf4b721d416861ade29d207f9c1a0dcd698f9c88
buckster12 <roboks@mail.ru>
2018-10-19 18:36:23 +0300

    added enable/disable checkbox added datepicker inputs

--- background.js
+++ background.js
@@ -2,42 +2,51 @@
 function onInstalled() { }
 
 function onAlarm(alarm) {
-    // console.log('alarm');
-    console.log(alarm);
+    // default set to false, to prevent of infinity of windows
+    var enabled = false;
 
-    // switch (alarm.name) {
-    //     case 'updatePhotos':
-    //         break;
-    //
-    //     default:
-    //         break;
-    // }
+    // check if extension enabled
+    chrome.storage.local.get(['enableExtension'], function (result) {
+        enabled = result.enableExtension;
 
-    var betweenDates = ['01.02', '31.03'];
+        if(enabled == false) {
+            console.log('exension disabled');
+            return false;
+        }
 
+        var betweenDates = ['01.02', '31.03'];
 
-    var dateFrom = '0802';
-    var dateTo = '2102';
-    var from = "MOW";
-    var to = "CMB";
+        var dateFrom = '0802';
+        var dateTo = '2102';
+        var from = "MOW";
+        var to = "CMB";
 
-    var action_url = "https://www.aviasales.ru/search/"+from+dateFrom+to+dateTo+"1";
-    chrome.tabs.create({ url: action_url });
+        chrome.storage.local.get(['myRandomKey'], function(result) {
+            console.log('Value currently is ' + result.key);
+        });
+        var action_url = "https://www.aviasales.ru/search/"+from+dateFrom+to+dateTo+"1";
+        var creating = chrome.tabs.create({ url: action_url });
+        // console.log("inside function: "+enabled);
+    });
+    // console.log("outside function: "+enabled);
 }
 
-
-// event: called when Chrome first starts
-function onStartup() {
-    // CREATE ALARMS HERE
-// ...
+function onError() {
+    console.log('error during tab opening');
 }
+
+function onCreated() {
+    console.log('tab created');
+}
+
+function onStartup() { }
 
 
 // listen for extension install or update
-chrome.runtime.onInstalled.addListener(onInstalled);
+// chrome.runtime.onInstalled.addListener(onInstalled);
 
 // listen for Chrome starting
-chrome.runtime.onStartup.addListener(onStartup);
+// chrome.runtime.onStartup.addListener(onStartup);
 
 // listen for alarms
 chrome.alarms.onAlarm.addListener(onAlarm);
--- popup.js
+++ popup.js
@@ -1,10 +1,49 @@
 $(document).ready(function () {
 
+    $('.datepicker').datepicker({
+        autoclose: true
+    });
+
+    // set checkbox when window loaded
+    chrome.storage.local.get(['enableExtension'], function (result) {
+        $('#enableExtension').prop("checked", result.enableExtension);
+        $("#log").text(result.enableExtension);
+    });
+
+    var $jsonData;
+
+    // TODO: file get json airports
+    var items = ['<option></option>'];
+    $.getJSON("airports.json", function(data) {
+        $.each( data, function( key, val ) {
+            items.push( "<option id='" + key + "'>" + val.name + "</option>" );
+        });
+    });
+    $( "<select/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+    }).appendTo( "#airport_from" );
+
+    // TODO: assign airports to select menu
+
+
+    $('#enableExtension').on('change', function () {
+        var value = $(this).is(":checked");
+        chrome.storage.local.set({'enableExtension':value}, function(result) {});
+    });
+
     // chrome.alarms.onAlarm.addListener(onAlarm);
 
+    $('#getValue').on('click', function () {
+        chrome.storage.local.get(['duration'], function(result) {
+            // $('#enableExtension').val(result.key);
+            $('#log').text(result.duration);
+        });
+
+    });
+
     $('#buttonTest').on('click', function () {
-
-
+        
         // chrome.alarms.create('checkNewTasks', {
             // when: 1000,
             // periodInMinutes: 1
@@ -15,12 +54,16 @@ $(document).ready(function () {
         var from = "MOW";
         var to = "CMB";
 
-        var action_url = "https://www.aviasales.ru/search/"+from+dateFrom+to+dateTo+"1";
-        chrome.tabs.create({ url: action_url });
+        // var action_url = "https://www.aviasales.ru/search/"+from+dateFrom+to+dateTo+"1";
+        // chrome.tabs.create({ url: action_url });
     });
 
     $('#saveSettings').on('click', function () {
         var duration = $('#durationInput').val();
+
+        var get
+
+        $('#log').text(duration);
 
         chrome.storage.local.set({duration: duration}, function() {
             $('#result').html('<span>saved</span>');
@@ -34,13 +77,9 @@ $(document).ready(function () {
 
 });
 
-// chrome.storage.local.set({key: value}, function() {
-//     console.log('Value is set to ' + value);
+// chrome.storage.local.get(['duration'], function(result) {
+//     console.log('Value currently is ' + result.key);
 // });
-
-chrome.storage.local.get(['duration'], function(result) {
-    console.log('Value currently is ' + result.key);
-});
 
 // Run alarm every minute
 // chrome.alarms.create('checkNewTasks', { periodInMinutes: 5 });
